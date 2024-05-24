5b3755ba85808e3bdfa7dab9c8019f6117d431bd
dovanthanhgm <149997073+dovanthanhgm@users.noreply.github.com>
2024-05-24 18:21:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dovanthanhgm/react)
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <div className="text-3xl font-bold underline">lorem</div>
+    </div>
+  );
+}
+
+export default App;
